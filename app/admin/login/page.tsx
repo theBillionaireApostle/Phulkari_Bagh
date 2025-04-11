4861d44a7eab5fdc7df7a1909069db82f2477c98
Mr.Bright <39347404+theBillionaireApostle@users.noreply.github.com>
2025-04-11 22:13:04 +0530

    Update page.tsx

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -102,7 +102,7 @@ export default function AdminLoginPage() {
     }
 
     try {
-      const res = await fetch("http://localhost:3000/api/admin/login", {
+      const res = await fetch("https://phulkari-bagh-backend.vercel.app/api/admin/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
